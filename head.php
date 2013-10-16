cc45ea5ba312ccd2026a1da68bd8454d2a865dfb
Ian <ian@Ian-Coatess-iMac.local>
2013-10-16 16:56:49 +0100

    updates

--- head.php
+++ head.php
@@ -2,3 +2,6 @@
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="citation.js"></script>
+	<!--[if lt IE 9]>
+	<script src="dist/html5shiv.js"></script>
+	<![endif]-->
