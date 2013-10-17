3e6ac78827773fa674288a5143d70d095941c5fe
Ian <ian@Ian-Coatess-iMac.local>
2013-10-17 18:53:59 +0100

    update

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "head.php"; ?>
+	<title>Home</title>
+</head>
+<body>
+
+<?php 
+	include "header.php";
+	include "nav.php";
+	include "funct.inc.php";
+	
+?>
+
+<div class='main'>
+
+<h1>Welcome</h1>
+
+</div>
+
+</body>
+
+</html>
