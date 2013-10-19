efc1f33cd63f29df3f086c6dbfd06b2c78367265
Ian <ian@Ian-Coatess-iMac.local>
2013-10-19 15:22:05 +0100

    homepage update

--- home.php
+++ home.php
@@ -14,9 +14,10 @@
 ?>
 
 <div class='main'>
-	<div class='main'>
+	<div class='welcome'>
 		<h1>Welcome</h1>
 		<p>Quickly search for similar papers to boost citations.</p>
+		<p>You can export from Web of Science and upload here. Then, you can search for similar papers that you have recently published and suggest to authors that they might like to cite these papers with the automatcically generated email.</p>
 	</div>
 </div>
 
