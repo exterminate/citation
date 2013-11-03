cf8032019e41d8e38cda363316c2d7625dd94a5e
Ian <ian@Ian-Coatess-iMac.local>
2013-11-03 11:23:14 +0000

    update to search function including lots of new stopwords

--- funct.inc.php
+++ funct.inc.php
@@ -12,10 +12,44 @@
 	
 	function searchMatch($dbarray,$title,$descr) { // search database
 
-		$before = array(",",";",":","*","<",">","'","."," and "," to "," the "," for "," it "," with ","-"); 
-		$after = array("","","","","","","",""," "," "," "," "," "," "," ");
-
-
+		$before = array(",",";",":","*","<",">","'","."); 
+		$after = array("","","","","","","","");
+		
+		$stopwords = array(" about", " above", " across", " afterwards", " after", " against", " again", " all ", " almost", "  along", "already", " also", " although", " always", " amongst", " among",  " amoungst", " amount", " am ", " and ", " another", " anyhow", " anyone", " anything", " anyway", " anywhere", " any ", " are ", " around", " as ",  " at ", 
+		" back"," be "," became", " because"," becomes"," becoming", " been", " beforehand", " before", " behind", " being", " below", " besides", " beside", " between", " beyond", " bill", " both", " bottom"," but ", " by ", " best ",
+		" call", " cannot ", " can ", " cant ", " con ", " co ", " could", " couldnt", " cry ", 
+		" describe", " detail", " done", " down ", " do ", " due", " during"," de ", " different ",
+		" each "," eg ", " eight", " either", " eleven"," elsewhere", " else ", " empty", " enough", " etc.", " even", " everyone", " every", " everything", " everywhere", " ever ", " except ", " exceptionally ",
+		" few", " fifteen", " fill", " find", " fire", " first", "five", " former", " formerly", " forty", " for ", " found", " four", " from", " front", " full", " further", 
+		" get ", " go ", 
+		" had ", " has ", " hasnt", " have",  " hence", " her", " hereafter", " hereby", " herein", " hereupon", " here ", " however"," how ", " hundred", 
+		" ie. ", " if ",  " indeed", " interest", " into", " in ", " is ", " itself ", " its ", " it ", " including ",
+		" keep", 
+		" last", " latterly", " latter", " least", " less", " ltd", 
+		" made", " many", " may",  " meanwhile", " me ", " might", " mill", " mine", " moreover", " more", " mostly", " most", " move", " much", " must", " my ", " myself", 
+		" namely", " name ", " neither", " nevertheless", " new ", " never", " next", " nine",  " nobody", " none", " noone", " nor", " nothing", " not", " nowhere", " now", " no ", 
+		" off ", " of ", " often", " on ", " once", " one ", " only ", " onto ", " or ", " others", " other ", " otherwise", " ours ", " our ", " ourselves", " out ", " over ", " own ",
+		" part ", " per ", " perhaps", " please", " put ", " present ",
+		" rather", " re ", 
+		" same", " see ", " seemed", " seem ", " seeming", " seems", " serious", " several", " she ", " should", " show ", " side ", " since ", " sincere", " six ", " sixty", " so ", " some ", " somehow", " someone", " something", " sometimes", " sometime", " somewhere", " still", " such", " system", " study ",
+		" take ", " ten ", " than ", " that ", " the ", " their ", " them ", " themselves", " then ", " thence", " there ", " thereafter", " thereby", " therefore", " therein", " thereupon", " these ", " they ", " thick ", " thin ", " third", " this ", " those ", " though ", " three ", " through ", " throughout ", " thru ", " thus ", " to ", " together", " too ", " top ", " towards", " toward", " twelve", " twenty", " two ", 
+		" un ", " under ", " until ", " up ", " upon ", " us ", " using ",
+		" very ", " via ", 
+		" was ", " we ", " well ", " were ", " what ", " whatever ", " when ", " whence ", " whenever ", " where ", " whereafter ", " whereas", " whereby", " wherein", " whereupon", " wherever", " whether", " which", " while", " whither", " who ", " whoever", " whole", " whom", " whose",  " why ", " will ", " with ", " within", " without", " would",
+		" yourselves", " yet ", " you ", " your ", " yours ", " yourself", " the ", " a "
+		);
+		
+		$title = str_replace($stopwords," ",$title);
+		$descr = str_replace($stopwords," ",$descr);
+		
+		/*for($i = 0; $i < count($stopwords); $i++){
+		    $title = str_replace($stopwords[$i]," ", strtolower($title));    
+		}
+		
+		for($i = 0; $i < count($stopwords); $i++){
+			$descr = str_replace($stopwords[$i]," ", strtolower($descr));    
+		}*/
+		
 		$titArray = explode(" ", trim(str_replace($before,$after,$title)));
 		$desArray = explode(" ", trim(str_replace($before,$after,$descr)));
 
